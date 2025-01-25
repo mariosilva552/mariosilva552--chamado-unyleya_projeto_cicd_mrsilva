cbf3e0ef9d045ba2ef51e7988c5d1daaaff0214b
mariosilva552 <denilsonpereira645@gmail.com>
2025-01-25 19:41:48 -0300

    Atualizando terraform

--- Terraform/Azure/main.tf
+++ Terraform/Azure/main.tf
@@ -7,7 +7,6 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 
@@ -48,13 +47,42 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
+
 resource "azurerm_linux_virtual_machine" "example" {
   name                = "example-machine"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
   admin_username      = "mrsilva"
-  admin_password      = "sft@L0cal"
+  disable_password_authentication = true
+
+  admin_ssh_key {
+    username   = "mrsilva"
+    public_key = file("C:/Users/mrsilva/.ssh/id_rsa.pub")
+  }
+
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
